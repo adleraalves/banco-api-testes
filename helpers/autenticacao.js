9f5a76baec0e5497f938ab7004c349f0777b7e45
Adler <adleralves@gamil.com>
2026-03-24 18:44:24 -0300

    Adicionando fixtures para gerir os corpos das requisições

--- helpers/autenticacao.js
+++ helpers/autenticacao.js
@@ -1,15 +1,17 @@
-const  request  =  require ( 'supertest' ); 
+const request  =  require ( 'supertest' ); 
+const postLogin = require('../fixtures/postLogin.json')
 require ( 'dotenv' ) . config ( )
 
 //Capturar o token
 const obterToken = async (usuario,senha) => {
+    const bodyLogin = {...postLogin}
+    bodyLogin.username = usuario
+    bodyLogin.senha = senha
+
     const respostaLogin = await request(process.env.BASE_URL)
         .post('/login')
         .set('Content-Type', 'application/json')
-        .send({
-            'username': usuario,
-            'senha': senha
-        })
+        .send(bodyLogin)
 
     //console.log(respostaLogin.status);
     //console.log(respostaLogin.body);
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,6 +1,7 @@
 const  request  =  require ( 'supertest' ) ; 
 const {expect} = require ('chai');
 const {obterToken} = require('../helpers/autenticacao.js')
+const postTransferencias = require('../fixtures/postTransferecias.json')
 require ( 'dotenv' ) . config ( )
 
 describe('Transferências', () => {
@@ -13,32 +14,27 @@ describe('Transferências', () => {
         })
 
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$10,00', async() => {
+            const bodyTransferencias = {...postTransferencias}
+
             const resposta = await request(process.env.BASE_URL)
             .post('/transferencias')
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${token}`)
-            .send({
-                'contaOrigem': 1,
-                'contaDestino': 2,
-                'valor': 11,
-                'token': ""
-                })
+            .send(bodyTransferencias)
 
             console.log(resposta.body);
 
             expect(resposta.status).to.equal(201);
         })
         it('Deve retornar erro com 422 quando o valor da transferência for baixo de R$10,00', async() => {
+            const bodyTransferencias = {...postTransferencias}
+            bodyTransferencias.valor = 7
+
             const resposta = await request(process.env.BASE_URL)
             .post('/transferencias')
             .set('Content-Type', 'application/json')
             .set('Authorization', `Bearer ${token}`)
-            .send({
-                contaOrigem: '1',
-                contaDestino: '2',
-                valor: 7,
-                token: ""
-                })
+            .send(bodyTransferencias)
 
             console.log(resposta.body);
 
