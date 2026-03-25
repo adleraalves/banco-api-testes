925db0542912514954d82abf38a1841846649e11
Adler <adleralves@gamil.com>
2026-03-24 21:41:29 -0300

    Adicionei testes para GET transferencias total e individual

--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -5,13 +5,14 @@ const postTransferencias = require('../fixtures/postTransferecias.json')
 require ( 'dotenv' ) . config ( )
 
 describe('Transferências', () => {
-    describe('POST /transferencias', () => {
-        let token
+    let token
 
-        beforeEach( async () => {
-            //Capturar o token
-            token = await obterToken('julio.lima', '123456');
-        })
+    beforeEach( async () => {
+        //Capturar o token
+        token = await obterToken('julio.lima', '123456');
+    })
+
+    describe('POST /transferencias', () => {
 
         it('Deve retornar sucesso com 201 quando o valor da transferência for igual ou acima de R$10,00', async() => {
             const bodyTransferencias = {...postTransferencias}
@@ -41,5 +42,38 @@ describe('Transferências', () => {
             expect(resposta.status).to.equal(422);            
         })
 
+    })
+
+    describe('GET /transferencias/{id}', () => {
+        it('Deve retornar sucesso com 200 e dados iguais ao registro de transferência contida no banco de dados quando o id for válido', async () => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias/10')
+                .set('Authorization', `Bearer ${token}`)
+
+            console.log(resposta.status);
+            console.log(resposta.body);
+
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.id).to.equal(10)
+            expect(resposta.body.id).to.be.a('number')
+            expect(resposta.body.conta_origem_id).to.equal(1)
+            expect(resposta.body.conta_destino_id).to.equal(2)
+            expect(resposta.body.valor).to.equal(11.00)
+        })
+    })
+
+    describe('GET /transferencias', () => {
+        it('Deve retornar 10 elementos na paginação quando informar limite de 10 registros', async() => {
+            const resposta = await request(process.env.BASE_URL)
+                .get('/transferencias?page=1&limit=10')
+                .set('Authorization', `Bearer ${token}`)
+
+            console.log(resposta.status);
+            console.log(resposta.body);
+
+            expect(resposta.status).to.equal(200)
+            expect(resposta.body.limit).to.equal(10)
+            expect(resposta.body.transferencias).to.have.length(10)
+        })
     })
 })
